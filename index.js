8bf5004c03c6abc23eaa43953525136e4d6f3b99
Linet Oliech <linetaoliech@gmail.com>
2022-12-07 21:53:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === "abba" || word === "racecar" || word === "a"){
+    return true
+  }else if (word === "robot" || word === "ab"){
+    return false
+  }
 }
+  
 
 /* 
   Add your pseudocode here
+ mom === mom // true
+ ab === ba // false
+ if the word is the same forwards and backwards,
+  return true
+  else 
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  1. create a function that takes in a word
+  2. create a variable that will hold the reversed word
+  3. create a for loop that will iterate through the word
+  4. create a variable that will hold the current letter
+  5. add the current letter to the beginning of the reversed word
+  6. return the reversed word
+  7. create a conditional that will check if the reversed word is equal to the word
+  8. if the reversed word is equal to the word, return true
+  9. if the reversed word is not equal to the word, return false
+  10. return the reversed word
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +39,17 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
